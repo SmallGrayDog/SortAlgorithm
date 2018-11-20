83af1cb373bf463cc09b1fc79b0f551f1cd8f78f
SmallGrayDog <32826098+SmallGrayDog@users.noreply.github.com>
2018-11-20 15:30:09 +0800

    Update Program.cs

--- SortAlgorithm/Program.cs
+++ SortAlgorithm/Program.cs
@@ -13,3 +13,25 @@ namespace SortAlgorithm
         }
     }
 }
+
+lua scripts
+-- --二分查找
+-- function UI_WeatherMain:Binary_Search(_CurDate)
+-- 	if _CurDate ~= 0000 then 
+-- 		local _CureDateNum = tonumber(_CurDate)
+-- 		local _start = 1
+-- 		local _end = #TB_WEATHER_CONFIG
+-- 		while _start <= _end do 
+-- 			local _mid = math.floor( (_start + _end) / 2)
+-- 			if _CureDateNum > TB_WEATHER_CONFIG[_mid].Date then 
+-- 			_start = _mid + 1 
+-- 			elseif _CureDateNum < TB_WEATHER_CONFIG[_mid].Date then 
+-- 				_end = _mid - 1 
+-- 			else 
+-- 				return  TB_WEATHER_CONFIG[_mid]
+-- 			end
+-- 		end
+-- 		return nil
+-- 	end
+-- 	return nil
+-- end
